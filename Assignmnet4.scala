fd87c0dae14a71bd6b1276a32c4c75f374c89e23
Chathura Janaranjana Wanniarachchi <wachathuraj@gmail.com>
2020-07-17 05:24:02 +0530

    Update Code with Scala map functions
    Each capitalized words can encrypt and decrypt with this code.

--- Assignmnet4.scala
+++ Assignmnet4.scala
@@ -1,55 +1,52 @@
-object Assignment4{
-  var shiftCount=10;
+object fpMod {
   def main(args: Array[String]): Unit = {
-    println("----Initiating System----");
-    print("Input Shift Count:\n>>>");
-    shiftCount=scala.io.StdIn.readInt()%26;
-    printf("Real Shift Count is: %d\n",shiftCount);
-    println("--- Welcome to Caesar cipher ---");
-    var key=0;
-    do{
-      println("Press 1 for Encrypt");
-      println("Press 2 for Decrypt");
-      print("Press 0 for Exit\n>>>");
-      key=scala.io.StdIn.readInt();
-      if(key==1){
-        print("Input String You Need to Encrypt:\n>>>");
-        var text=scala.io.StdIn.readLine().toString;
+    var shiftCount = 0
+    println("----Initiating System----")
+    print("Input Shift Count:\n>>>")
+    shiftCount = scala.io.StdIn.readInt() % 26
+    printf("Real Shift Count is: %d\n", shiftCount)
+    println("--- Welcome to Caesar cipher ---")
+    var key = 0
+    do {
+      println("Press 1 for Encrypt")
+      println("Press 2 for Decrypt")
+      print("Press 0 for Exit\n>>>")
+      key = scala.io.StdIn.readInt()
+      if (key == 1) {
+        print("Input String You Need to Encrypt:\n>>>")
+        val text = scala.io.StdIn.readLine().toString.toUpperCase
         print("Encrypted>>>")
-        Encryption(text, shiftCount);
-        println("\n");
-      }else if(key==2){
-        print("Input String You Need to Decrypt:\n>>>");
-        var text=scala.io.StdIn.readLine().toString;
+        Encryption(text, shiftCount)
+        println("\n")
+      } else if (key == 2) {
+        print("Input String You Need to Decrypt:\n>>>")
+        val text = scala.io.StdIn.readLine().toString
         print("Decrypted>>>")
-        Decryption(text, shiftCount);
-        println("\n");
-      }else{
-        key=0;
+        Decryption(text, shiftCount)
+        println("\n")
+      } else {
+        key = 0
       }
-    }while(key!=0);
+    } while (key != 0)
   }
 
-  def Encryption(text: String,shiftCount:Int):Unit={
-    for(x<-text){
-      print(charShift(x,shiftCount).toChar);
-    }
+  def Encryption(text: String, sc: Int): Unit = {
+    charShift(text, sc)
   }
 
-  def Decryption(text: String,shiftCount:Int):Unit={
-    for(x<-text){
-      print(charShift(x,shiftCount*(-1)).toChar);
-    }
+  def Decryption(text: String, sc: Int): Unit = {
+    charShift(text, -sc)
   }
 
-  def charShift(letter:Char,shiftCount:Int):Int={
-    var assciVal=letter.toInt;
-    if((assciVal>96 && assciVal<123) || (assciVal>64 && assciVal<91)){
-      assciVal=assciVal+shiftCount;
-      if(!((assciVal>96 && assciVal<123) || (assciVal>64 && assciVal<91))){
-        assciVal=assciVal+ (if(shiftCount>0)-26 else +26);
+  def charShift(text: String, sc: Int): Unit = {
+    val textList = text.split(" ")
+    val shiftChar = (c: Char) => if (c > 96 && c < 123 || c > 64 && c < 91) c.toInt + sc % 26 else c
+    val shiftFilter = (c: Int) => if (!(c > 64 && c < 91)) c + (if (sc > 0) -26 else +26) else c
+    for (i <- textList) {
+      for (j <- i.map(shiftChar).toList.map(shiftFilter)) {
+        print(j.toChar)
       }
+      print(" ")
     }
-    assciVal
   }
 }
